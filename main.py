bbd87b4f10c3312353db3cd5e8b4e14d934e2bdf
Abel Gómez Méndez <abelmetaltele@gmail.com>
2024-01-25 06:40:36 -0500

    commit app funcionando basico

--- main.py
+++ main.py
@@ -1,22 +1,42 @@
 from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+import pymongo
 from pymongo import MongoClient
+import pandas as pd
 
 app = FastAPI()
 templates = Jinja2Templates(directory='./Templates')
-lista_comentarios_usuarios=[]
+# Crea una conexión al servidor de MongoDB
+client = pymongo.MongoClient("mongodb://localhost:27017/")
 
-@app.get('/formulario', response_class=HTMLResponse)
-def mostrar_comentarios(peticion:Request):
+# Selecciona la base de datos
+db = client["citas"]
+
+# Selecciona la colección
+coleccion = db["citas"]
+
+# Busca todos los documentos en la colección
+query = coleccion.find()
+
+df = pd.DataFrame(query).drop('_id', axis=1)
+app = FastAPI()
+templates = Jinja2Templates('./Templates')
+
+@app.get('/formulario_inicio', response_class=HTMLResponse)
+def leer_formulario(peticion:Request):
     return templates.TemplateResponse('index.html',{'request':peticion})
 
-@app.post('/enviar_comentario')
-def comentar(peticion:Request, comentario:str=Form(...)):
-    lista_comentarios_usuarios.append(comentario)
-    return templates.TemplateResponse('index.html',{'request':peticion, 'lista_comentarios_usuarios':lista_comentarios_usuarios})
-
-@app.post('/resetear')
-def resetear_comentarios(peticion:Request):
-    lista_comentarios_usuarios.clear()
-    return templates.TemplateResponse('index.html',{'request':peticion})
+@app.post('/enviar_categoria')
+def enviar_categoria(peticion:Request, categoria:str=Form(...)):
+    try:
+        if categoria == '-':
+            return f'Seleccione una categoria'
+        elif categoria not in df['categoria'].values:
+            return f'La categoria {categoria} no existe'
+        df_filtrado = df[df['categoria'] == categoria]
+        serie_aleatoria = df_filtrado.sample(n=1).iloc[0]
+        cita_devuelta = serie_aleatoria['cita']
+        return templates.TemplateResponse('index.html',{'request':peticion, 'cita_devuelta':cita_devuelta})
+    except Exception as e:
+        return f'Error procesando el archivo{e}'
